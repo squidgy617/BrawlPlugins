f151ffeeee7e697e744e835c8d601a484c4443b6
ilazoja <ilazoja@uwaterloo.ca>
2025-06-27 21:53:51 -0700

    Sync with Kape's fork

--- AsyncRSP/source/sel_char_load_thread.cpp
+++ AsyncRSP/source/sel_char_load_thread.cpp
@@ -33,8 +33,8 @@ selCharLoadThread::selCharLoadThread(muSelCharPlayerArea* area)
     m_playerArea = area;
     m_dataReady = false;
     m_isRunning = false;
-    m_updateEmblem = false;
-    m_updateName = false;
+    m_shouldUpdateEmblem = false;
+    m_shouldUpdateName = false;
     m_lastSelectedCharKind = -1;
 
     m_fileBuffer = gfHeapManager::alloc(threadBufferHeap, threadBufferSize);
@@ -140,13 +140,17 @@ void selCharLoadThread::main()
 }
 
 
-void selCharLoadThread::requestLoad(int charKind)
+void selCharLoadThread::requestLoad(int charKind, bool hasCsp)
 {
     if (isNoLoadSelchKind(charKind)) {
         m_toLoad = -1;
     }
     else {
         m_toLoad = charKind;
+        if (!hasCsp) {
+            m_shouldUpdateEmblem = false;
+            m_shouldUpdateName = false;
+        }
     }
     m_lastSelectedCharKind = charKind;
     m_dataReady = false;
@@ -161,8 +165,8 @@ void selCharLoadThread::reset()
 //    }
 
     m_dataReady = false;
-    m_updateEmblem = false;
-    m_updateName = false;
+    m_shouldUpdateEmblem = false;
+    m_shouldUpdateName = false;
     m_toLoad = -1;
 }
 
